7d09beb61e6a2da3ca4a67c6ccc894702a858799
Dan Behlings <nevenall@hotmail.com>
2020-10-14 14:10:11 -0700

    fix css for sheet tabs

--- module/actor-sheet.js
+++ module/actor-sheet.js
@@ -192,6 +192,8 @@ export class ShadowrunActorSheet extends ActorSheet {
                      callback: (html) => {
                         let rawFormData = new FormData(document.querySelector('form.test-options'))
                         let data = Object.fromEntries(rawFormData.entries())
+                        // todo - if the option to reroll fails is true, then we want to add a r<5 right the d6
+                        // is reroll and explode a 
                         let roll = new Roll(`${test.pool(data.applyStatus) + parseInt(data.adjustPool)}d6${data.explode ? 'x' : ''}cs>4${data.threshold ? `ms>=${parseInt(data.threshold)}` : ''}`).roll()
 
                         let options = {
